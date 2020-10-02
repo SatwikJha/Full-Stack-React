70c179009ee3f604116206d34f8e46d3cbb892b6
Gaurav <jha.gaurav70@gmail.com>
2020-10-02 19:47:35 +0530

    Added React Modules

---
+++ React/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Main from './components/MainComponent';
+import {BrowserRouter} from 'react-router-dom';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+  };
+
+  render () {
+    return(
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
+    );
+  };
+  
+}
+
+export default App;
